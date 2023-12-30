1a589bdfcd53851343a05ec8eaddde00199bf1a5
Partho Bhowmick <p.s.bhowmick@gmail.com>
2023-12-29 21:58:42 -0600

    python-operator
    Task CR fixed so it's easy to create a pod out of it.
    
    Task controller can now validate the spec of the task can be used to generate a pod

--- kmambo-operator/src/kmambo/task_controller.py
+++ kmambo-operator/src/kmambo/task_controller.py
@@ -1,9 +1,35 @@
+import json
 import logging
+import os
 import kopf
+from kubernetes.client import V1Namespace
+from kubernetes import config, client
 
 TASK = "task"
 
 
+@kopf.on.startup()
+async def startup_func(logger, **kwargs):
+    if os.getenv("KUBERNETES_SERVICE_HOST"):
+        config.load_incluster_config()
+    else:
+        # assume running outside cluster AND kubeconfig is available
+        config.load_kube_config()
+
+
 @kopf.on.create(TASK)
-def task_on_create(spec, name, namespace, logger: logging.Logger, body, **kwargs):
+def task_on_create(spec: kopf.Spec, name, namespace, logger: logging.Logger, **kwargs):
     logger.info(f"name: {name}, namespace: {namespace}, spec:{spec}")
+    logger.info(f"type(spec): {type(spec)}")
+    # logger.info(f"kwargs:{kwargs}")
+    v1 = client.CoreV1Api()
+    body = {
+        'apiVersion': 'v1',
+        'kind': 'Pod',
+        'metadata': {
+            'name': name
+        },
+        'spec': eval(f"{spec['template']}")
+    }
+    resp = v1.create_namespaced_pod(namespace="default", body=body, dry_run="All")
+    logger.info(f"resp: {resp}")
