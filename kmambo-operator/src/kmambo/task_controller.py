bd4f11439e12b4935ca1b1ea7afe07c265c02891
Partho Bhowmick <p.s.bhowmick@gmail.com>
2023-12-30 10:22:48 -0600

    python-operator
    Added makefile and Dockerfile
    Pre-cursor to skaffolding

--- kmambo-operator/src/kmambo/task_controller.py
+++ kmambo-operator/src/kmambo/task_controller.py
@@ -2,23 +2,25 @@ import json
 import logging
 import os
 import kopf
-from kubernetes.client import V1Namespace
 from kubernetes import config, client
 
 TASK = "task"
 
 
 @kopf.on.startup()
-async def startup_func(logger, **kwargs):
+async def startup_func(settings, **_):
     if os.getenv("KUBERNETES_SERVICE_HOST"):
+        settings.admission.server = kopf.WebhookServer(addr='0.0.0.0', port=8080)
         config.load_incluster_config()
     else:
         # assume running outside cluster AND kubeconfig is available
+        settings.admission.server = kopf.WebhookAutoServer(port=8080)
+        settings.admission.managed = 'auto.kopf.dev'
         config.load_kube_config()
 
 
-@kopf.on.create(TASK)
-def task_on_create(spec: kopf.Spec, name, namespace, logger: logging.Logger, **kwargs):
+@kopf.on.validate(TASK)
+async def task_on_create(spec: kopf.Spec, name, namespace, logger: logging.Logger, **kwargs):
     logger.info(f"name: {name}, namespace: {namespace}, spec:{spec}")
     logger.info(f"type(spec): {type(spec)}")
     # logger.info(f"kwargs:{kwargs}")
@@ -31,5 +33,8 @@ def task_on_create(spec: kopf.Spec, name, namespace, logger: logging.Logger, **k
         },
         'spec': eval(f"{spec['template']}")
     }
-    resp = v1.create_namespaced_pod(namespace="default", body=body, dry_run="All")
+    try:
+        resp = v1.create_namespaced_pod(namespace="default", body=body, dry_run="All")
+    except Exception as e:
+        raise kopf.AdmissionError(f"Unable to create Task {e}")
     logger.info(f"resp: {resp}")
